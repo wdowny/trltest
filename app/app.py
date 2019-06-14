237616061b88d7efe122e2172143956df898a9a3
wd <wdowny@mail.ru>
2019-06-14 23:52:34 +1000

    talkative app

--- app/app.py
+++ app/app.py
@@ -32,7 +32,7 @@ class Servant(BaseHTTPRequestHandler):
         self.send_response(404)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('Sorry, nothing was found by your request.'.encode())
+        self.wfile.write('Sorry, nothing was found by your request. <a href="/">Come back.</a>'.encode())
         self.counters[404]+=1
 
     def do_GET(self):
@@ -42,7 +42,13 @@ class Servant(BaseHTTPRequestHandler):
             self.defaultResponse()
         
     def evt_index(self):
-        self.httpResponse('Hello, TR Logic!<br>Pirates of the Caribbean welcome you!')
+        self.httpResponse('''
+Hello, TR Logic!
+<br>
+Pirates of the Caribbean welcome you!
+<br><br>
+Here is <a href="/metrics">link to metrics</a>. And <a href="/nowhere">this link</a> is broken. 
+''')
         
     def evt_metrics(self):
         res = 'rq_total ' + str(sum(self.counters.values())) +'\n'
